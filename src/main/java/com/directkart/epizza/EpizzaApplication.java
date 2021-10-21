051a932504efd296894d4572f9392081af8c8ce5
Vivek <vivek.shukla@juspay.in>
2021-10-21 11:21:32 +0530

    first commit

---
+++ src/main/java/com/directkart/epizza/EpizzaApplication.java
@@ -0,0 +1,13 @@
+package com.directkart.epizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/directkart/epizza/EpizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.directkart.epizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
